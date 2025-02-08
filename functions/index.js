a6391d166a76ddda283f00089615d2b16de73289
ree-rishun <ree0432@gmail.com>
2025-02-08 14:14:13 +0900

    feature: CORS対策を追加

--- functions/index.js
+++ functions/index.js
@@ -23,6 +23,22 @@ import {
 
 const app = express()
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
 
 app.get(
   '/',
